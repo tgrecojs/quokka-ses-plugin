c512b195306e982f48d46aa340221c2c08f1c175
Thomas Greco <6646552+tgrecojs@users.noreply.github.com>
2023-04-13 00:40:02 -0400

    fix: attempting once again...

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 "use strict";
+const All = (x) => ({
+  value: x,
+  concat: (other) => All(x && other.value),
+});
+All.empty = () => All(true);
+
+const isFrozen = (x) => Object.isFrozen(x);
+const fold =
+  (monoid) =>
+  (data = []) =>
+    data.reduce((acc, val) => acc.concat(monoid(val)), monoid.empty());
+const foldAll = fold(All);
+const getValue = ({ value }) => value;
+
+getValue(foldAll(types)); //?
+
 module.exports = {
   before: () => {
-    const lockdown = require("@endo/init/legacy");
-    return !Object.isFrozen(Array) && lockdown();
+    const types = [Array, Object, Number, Map, WeakMap, BigInt, Intl].map(
+      isFrozen
+    );
+    if (getValue(foldAll(types))) {
+      const lockdown = require("ses");
+      lockdown({
+        domainTaming: "unsafe",
+        overrideTaming: "severe",
+        stackFiltering: "verbose",
+        errorTaming: "unsafe",
+      });
+    }
   },
 };
